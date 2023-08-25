5a8635ee399dc5d49f2dac731783d1197bf76a85
AlekseyStogovv <astogovv@gmail.com>
2023-08-25 18:25:37 +0300

    add filters and tests

--- books/store/tests/test_serializers.py
+++ books/store/tests/test_serializers.py
@@ -6,19 +6,19 @@ from store.serializers import BooksSerializer
 
 class BookSerializerTestcase(TestCase):
     def test_ok(self):
-        book_1 = Book.objects.create(name='test book 1', price=25)
-        book_2 = Book.objects.create(name='test book 2', price=125)
+        book_1 = Book.objects.create(name='Test book 1', price=25)
+        book_2 = Book.objects.create(name='Test book 2', price=55)
         data = BooksSerializer([book_1, book_2], many=True).data
         expected_data = [
             {
                 'id': book_1.id,
-                'name': 'test book 1',
+                'name': 'Test book 1',
                 'price': '25.00'
             },
             {
                 'id': book_2.id,
-                'name': 'test book 2',
-                'price': '125.00'
+                'name': 'Test book 2',
+                'price': '55.00'
             },
         ]
         self.assertEqual(expected_data, data)
