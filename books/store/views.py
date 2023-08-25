904d34f1ddccdb45569bdda4c87ea406c5c7844b
AlekseyStogovv <astogovv@gmail.com>
2023-08-25 12:35:59 +0300

    first commit

---
+++ books/store/views.py
@@ -0,0 +1,11 @@
+from rest_framework.viewsets import ModelViewSet
+
+from store.serializers import BookSerializer
+from store.models import Book
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
